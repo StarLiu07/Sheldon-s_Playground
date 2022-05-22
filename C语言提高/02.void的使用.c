53076ed61c460afa297c88dfc2daf5b2598a63e8
Sheldon <3087925669@qq.com>
2022-05-22 20:25:13 +0800

    typedef void sizeof的使用

---
+++ C语言提高/02.void的使用.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//void的使用
+//1.无类型  不可以通过void创建变量，原因是编译器不知道分配多少内存
+
+void test01()
+{
+    //void a = 10;
+}
+
+//c语言并不严谨，若没加限定，可能不符合语法规范但依旧能够编译成功
+//2.限定函数返回值 限定函数参数
+int func()
+{
+    return 10;
+}
+
+void test02()
+{
+    printf("%d\n",func());
+}
+
+//3.万能指针
+void test03()
+{
+    void* p = NULL;
+    printf("p的内存空间大小%d\n",sizeof(p));
+
+    int* pInt = NULL;
+    char* pChar = NULL;
+
+    //pChar = pInt;//报错不兼容
+    pChar = (char*)pInt;
+}
+
+int main()
+{
+    //printf("hello world\n");
+    //test02();
+    test03();
+    return 0;
+}
