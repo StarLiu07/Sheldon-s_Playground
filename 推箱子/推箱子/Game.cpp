46555303a4a5851184e52f89053a441df245eb07
Sheldon <3087925669@qq.com>
2022-08-13 22:53:01 +0800

    8.13

--- 推箱子/推箱子/Game.cpp
+++ 推箱子/推箱子/Game.cpp
@@ -42,3 +42,72 @@ int Game::Move(int map[M][N], char ch)
 	}
 	return step;
 }
+
+//界面打印
+//传入一个二维数组和一个关卡数
+void Game::Drop(int map[M][N], int c)
+{
+	cout << "\t\t" << "**********************第 " << c << " 关**************************" << endl;
+	cout << "\t\t" << "***************W-w:向上  S-s:向下*****************" << endl;
+	cout << "\t\t" << "***************A-a:向左  D-d:向右*****************" << endl;
+	cout << endl;
+
+	//嵌套循环是为了遍历这个二维数组
+	for (int i = 0; i < M; i++)
+	{
+		cout << "	";
+		for (int j = 0; j < N; j++)
+		{
+			switch (map[i][j])
+			{
+				//打印空地
+			case 0:
+				cout << " ";
+				break;
+				//打印墙壁
+			case 1:
+				cout << "■";
+				break;
+				//打印玩家
+			case 2:
+				cout << "♀";
+				posX = i;
+				posY = j;
+				break;
+				//打印箱子
+			case 3:
+				cout << "□";
+				break;
+				//打印终点
+			case 4:
+				cout << "○";
+				break;
+			case 6:
+				cout << "★";
+				posX = i;
+				posY = j;
+				break;
+			case 7:
+				cout << "●";
+				break;
+			default:
+				break;
+			}
+		}
+		cout << endl; //换行
+	}
+}
+
+//判断游戏胜利条件
+int Game::juide(int map[M][N])
+{
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (4 == map[i][j] || 6 == map[i][j]) //地图中还存在重点/终点+人
+				return 1;
+		}
+	}
+	return 0;
+}
